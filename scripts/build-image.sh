e24b8718bb9e2eb59cb3e7f4a61c6e40a5a42962
hippoom <hippoom@gmail.com>
2016-06-12 19:11:10 +0800

    CI enabled

---
+++ scripts/build-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "$script_dir"/common.sh #use quote here to compliant with space in dir
+
+rm -rf "$project_home"/build/version
+
+echo $BUILD_NUM > "$project_home"/build/version
+
+version=$(cat "$project_home"/build/version)
+
+docker build -t "$main_image:$version" -f "$project_home"/Dockerfile "$project_home"
